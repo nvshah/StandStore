2d148396400c22b3b21a6419e452800c30811184
nvshah <nipunshah6776@gmail.com>
2020-06-02 01:00:42 +0530

    Code formatting

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -2,18 +2,17 @@ import 'package:flutter/material.dart';
 
 import '../widgets/products_grid.dart';
 
-enum FilterOptions{
+enum FilterOptions {
   Favorites,
   All,
 }
+
 class ProductsOverviewScreen extends StatefulWidget {
-  
   @override
   _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
 }
 
 class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
-
   bool _showOnlyFavorites;
 
   @override
@@ -25,21 +24,25 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
           PopupMenuButton(
             //OPTIONS
             itemBuilder: (_) => [
-              PopupMenuItem(child: Text('Only Favorites'), value: FilterOptions.Favorites,),
-              PopupMenuItem(child: Text('Show All'), value: FilterOptions.All,),
+              PopupMenuItem(
+                child: Text('Only Favorites'),
+                value: FilterOptions.Favorites,
+              ),
+              PopupMenuItem(
+                child: Text('Show All'),
+                value: FilterOptions.All,
+              ),
             ],
             //ICON
             icon: Icon(Icons.more_vert),
-            onSelected: (FilterOptions selectedValue){
+            onSelected: (FilterOptions selectedValue) {
               setState(() {
-                if(selectedValue == FilterOptions.Favorites){
-                _showOnlyFavorites = true;
-              }
-              else{
-                _showOnlyFavorites = false;
-              }  
+                if (selectedValue == FilterOptions.Favorites) {
+                  _showOnlyFavorites = true;
+                } else {
+                  _showOnlyFavorites = false;
+                }
               });
-              
             },
           ),
         ],
@@ -48,4 +51,3 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
     );
   }
 }
-
