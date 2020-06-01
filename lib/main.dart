df93005c989dad3ef22e8a84372392d9666a5e07
nvshah <nipunshah6776@gmail.com>
2020-06-02 01:36:43 +0530

    -- Cart Implementation New provider for Cart related data

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import './screens/products_overview_screen.dart';
 import './providers/products.dart';
 import './screens/product_details_screen.dart';
+import './providers/cart.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,12 +13,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     //This allows to register a class so that we can listen in child widgets; so this make it a Provider
     //Only child widget which are listening will rebuild
-    return ChangeNotifierProvider.value(
-      //Same instance of provider for all child wdgets that are listening
-      //NOTE - In earlier version instead of create, builder argument was used to create provider instance
-      //create: (ctxt) => Products(),
-      //Here we are not using any context so utilising ChangeNotifierProvider.value
-      value: Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          //Same instance of provider for all child wdgets that are listening
+          //NOTE - In earlier version instead of create, builder argument was used to create provider instance
+          //create: (ctxt) => Products(),
+          //Here we are not using any context so utilising ChangeNotifierProvider.value
+          value: Products(),
+        ),
+        ChangeNotifierProvider.value(
+          //As cart is also being used at multiple screen in out app so it's provider is managed at root level here
+          value: Cart(),
+        )
+      ],
       child: MaterialApp(
         title: 'Shop App',
         theme: ThemeData(
