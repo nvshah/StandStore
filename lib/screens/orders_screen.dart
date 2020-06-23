b1a2b740ff6289654a5bb64aae8f40c06f71703d
nvshah <nipunshah6776@gmail.com>
2020-06-24 01:06:36 +0530

    - Order Screen From Stateful to Stateless Widget --> Using FutureBuilder() approach

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -5,48 +5,74 @@ import '../providers/orders.dart' show Orders;
 import '../widgets/order_item.dart';
 import '../widgets/app_drawer.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
 
-  @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
+//   @override
+//   _OrdersScreenState createState() => _OrdersScreenState();
+// }
 
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
+// class _OrdersScreenState extends State<OrdersScreen> {
+  //var _isLoading = false;
 
-  @override
-  void initState() {
-    //Kind of hack, here code inside Future.delayed() will get queued up at last
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      //wait till all orders are fetched from server
-      await Provider.of<Orders>(context, listen: false).fetchOrder();
-      setState(() {
-        _isLoading = false;
-      });
-    });
-    super.initState();
-  }
+  // //Runs once when screen gets rendered
+  // @override
+  // void initState() {
+  //   // //Kind of hack, here code inside Future.delayed() will get queued up at last
+  //   // Future.delayed(Duration.zero).then((_) async {
+  //   //   setState(() {
+  //   //     _isLoading = true;
+  //   //   });
+  //   //   //wait till all orders are fetched from server
+  //   //   await Provider.of<Orders>(context, listen: false).fetchOrder();
+  //   //   setState(() {
+  //   //     _isLoading = false;
+  //   //   });
+  //   // });
+
+  //   _isLoading = true;
+  //   Provider.of<Orders>(context, listen: false).fetchOrder().then((_) {
+  //     setState(() {
+  //       _isLoading = false;
+  //     });
+  //   });
+  //   super.initState();
+  // }
 
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
+    print('building orders ...');
+    //Inorder to avoid Infinite loop
+    //final orderData = Provider.of<Orders>(context);
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Orders'),
       ),
-      body: _isLoading
-          ? Center(
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchOrder(),
+        //dataSnapShot is value that will be recieved in future
+        builder: (ctxt, dataSnapShot) {
+          if (dataSnapShot.connectionState == ConnectionState.waiting) {
+            return Center(
               child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: orderData.orders.length,
-              itemBuilder: (ctxt, i) => OrderItem(orderData.orders[i]),
-            ),
+            );
+          } else if (dataSnapShot.error != null) {
+            return Center(
+              child: Text('Something goes Wrong !'),
+            );
+          } else {
+            //Here we are using Consumer Inorder to avoid Infinite loop created by FutureBuilder & Provider.of()
+            //Only re-build parts that requires re-building
+            return Consumer<Orders>(
+              builder: (ctxt, orderData, child) => ListView.builder(
+                itemCount: orderData.orders.length,
+                itemBuilder: (ctxt, i) => OrderItem(orderData.orders[i]),
+              ),
+            );
+          }
+        },
+      ),
       drawer: AppDrawer(),
     );
   }
